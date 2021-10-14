ece44e71c4e285ded4dbf4029156fb483be87af3
Nijatasgar99 <nijataas@code.edu.az>
2021-10-14 06:23:49 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Celsius.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kelvinCelsius
+{   
+      
+    class Celsius
+    {
+        public double Cels { get; set; }
+        public Celsius(double c)
+        {
+            Cels = c;
+        }
+        public static implicit operator Kelvin(Celsius celsius)
+        {
+            return new Kelvin(celsius.Cels + 273.15);
+        }
+    }
+    class Kelvin
+    {
+        public double Kelv { get; set; }
+        public Kelvin(double k)
+        {
+            Kelv = k;
+        }
+
+
+    }
+   
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace kelvinCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(10);
+            Kelvin kelvin = celsius;
+            Console.WriteLine(kelvin.Kelv);
+        }
+    }
+}
